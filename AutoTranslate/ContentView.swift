02342423eddbec50c6afabb3a9ea1b3dab17cb38
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-02-11 14:58:51 +0100

    Initial commit

--- AutoTranslate/ContentView.swift
+++ AutoTranslate/ContentView.swift
@@ -6,16 +6,14 @@
 //
 
 import SwiftUI
+import Translation
 
 struct ContentView: View {
+    @State private var input = "Hello, world!"
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        TextEditor(text: $input)
+            .font(.largeTitle)
     }
 }
 
