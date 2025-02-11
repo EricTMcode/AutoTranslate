d81fe54dfa510a384d0b8be0fe9d183d20f9e3fa
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-02-11 15:08:49 +0100

    Create method to translate text

--- AutoTranslate/ContentView.swift
+++ AutoTranslate/ContentView.swift
@@ -11,9 +11,27 @@ import Translation
 struct ContentView: View {
     @State private var input = "Hello, world!"
 
+    @State private var configuration = TranslationSession.Configuration(
+        source: Locale.Language(identifier: "en"),
+        target: Locale.Language(identifier: "es")
+        )
+
     var body: some View {
         TextEditor(text: $input)
             .font(.largeTitle)
+            .onChange(of: input) {
+                configuration.invalidate()
+            }
+            .translationTask(configuration, action: translate)
+    }
+
+    func translate(using session: TranslationSession) async {
+        do {
+            let result = try await session.translate(input)
+            print(result.targetText)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
