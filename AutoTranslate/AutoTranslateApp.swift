1af9e54ca650858ef141e983af880e99933d7ce5
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-02-11 14:53:37 +0100

    Initial Commit

---
+++ AutoTranslate/AutoTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoTranslateApp.swift
+//  AutoTranslate
+//
+//  Created by Eric on 11/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AutoTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
